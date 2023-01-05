365b4d05c9f689dd4da64ae5a98487f340795c9a
Mikko <realmiks27@gmail.com>
2023-01-04 19:05:19 -0800

    Create a clear button which deletes the current grid Used a for each loop to iterate through each grid item and remove it from its parent

--- script.js
+++ script.js
@@ -2,54 +2,51 @@ const body = document.querySelector('body');
 const button = document.querySelector('#button');
 
 const gridContainer = document.createElement('div');
-gridContainer.classList.toggle('.right-container');
-body.appendChild(gridContainer);    
+gridContainer.classList.toggle('grid-container');
 
+const rightContainer = document.querySelector('.right-container');
+rightContainer.appendChild(gridContainer);    
 
-
-
+const clearButton = document.querySelector('#clear-button');
 
 
 function setGrid() {
-    let n = 2
     button.addEventListener('click', () => {
-        n = prompt("How many squares do you want per side?");
+        let n = prompt("How many squares do you want per side?");
         createGrid(n);
 
+        //klkllk
         const gridItem = document.querySelectorAll('.grid-item');
         gridItem.forEach(item => item.addEventListener('mouseover', (e) => {
             changeColor(e.target);
         }
         ));
 
-        gridItem.forEach(item => item.addEventListener('mouseout', (e) => {
-            changeColor(e.target);
-        }
-        ));
+        
     })
     
 }
-
 setGrid();
 function createGrid(size) {
     for (let i = 0; i < size ** 2; i++) {
         gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
         gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
         const div = document.createElement('div');
-        div.classList.toggle('grid-item')
+        div.classList.toggle('grid-item');
         gridContainer.appendChild(div);
     }
 }
 
-
-
-
-//create a hover event listener that changes the divs color when the mouse hovers it
-// the function to run should be a function that adds a class to it which changes it color
-function changeColor(x) {
-    x.classList.toggle('change-color');
+function changeColor(element) {
+    element.classList.toggle('change-color');
 }
 
+//i need to clear the grid container
 
+clearButton.addEventListener('click', () => {
+    const div = document.querySelectorAll('.grid-item');
+    div.forEach(i => gridContainer.removeChild(i));
+ 
+})
 
 
