4159afb8c2863c17eabbd8f21cadbb5152f9a91a
Mikko <realmiks27@gmail.com>
2023-01-05 10:44:04 -0800

    Fixed bug where clicking 'click me' button more than once wouldn't work At first I had a 'clear-button' event listener in the 'clear' function when that wasn't necessary. All I had to do was just take out that 'clear-button' event listener and just have the code that deleted the current grid

--- script.js
+++ script.js
@@ -2,31 +2,34 @@ const body = document.querySelector('body');
 const button = document.querySelector('#button');
 
 const gridContainer = document.createElement('div');
-gridContainer.classList.toggle('grid-container');
+gridContainer.classList.add('grid-container');
 
 const rightContainer = document.querySelector('.right-container');
 rightContainer.appendChild(gridContainer);    
 
-const clearButton = document.querySelector('#clear-button');
 
 
+let n = 0;
 function setGrid() {
     button.addEventListener('click', () => {
-        let n = prompt("How many squares do you want per side?");
+        n = prompt("How many squares do you want per side?");
         createGrid(n);
-
+        
+        console.log(n);
         //klkllk
         const gridItem = document.querySelectorAll('.grid-item');
         gridItem.forEach(item => item.addEventListener('mouseover', (e) => {
             changeColor(e.target);
         }
         ));
-
+        clear();
         
     })
+
+    
     
 }
-setGrid();
+
 function createGrid(size) {
     for (let i = 0; i < size ** 2; i++) {
         gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -42,11 +45,25 @@ function changeColor(element) {
 }
 
 //i need to clear the grid container
+function clear() {
+        const div = document.querySelectorAll('.grid-item');
+        div.forEach(i => gridContainer.removeChild(i));
+        createGrid(n);
+        const gridItem = document.querySelectorAll('.grid-item');
+        gridItem.forEach(item => item.addEventListener('mouseover', (e) => {
+            changeColor(e.target);
+        }
+        )); 
+}
+setGrid();
 
-clearButton.addEventListener('click', () => {
-    const div = document.querySelectorAll('.grid-item');
-    div.forEach(i => gridContainer.removeChild(i));
- 
-})
+/*
+problem: after clicking the 'click me' button once,
+some of the squares will not work; they will be colored already and you can't change the color on them
 
 
+if 'click me' button gets clicked more than once, call the clear function to delete the current grid 
+and then call setgrid function to ask how they want the grid to be
+
+!! Need to make the code neater and better! improve it !!!!!
+*/
