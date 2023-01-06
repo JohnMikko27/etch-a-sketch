61bbaeed4ce2e03050ff72d76a939cea42fd2eb7
Mikko <realmiks27@gmail.com>
2023-01-05 19:36:10 -0800

    Change 'hover' event. So that hovering over a new div will produce generate a new color Created that new color with 3 random generator functions and pass those random numbers as the 'rgb' code

--- script.js
+++ script.js
@@ -7,22 +7,35 @@ gridContainer.classList.add('grid-container');
 const rightContainer = document.querySelector('.right-container');
 rightContainer.appendChild(gridContainer);    
 
+const blueButton = document.querySelector('.blue');
+const redButton = document.querySelector('.red');
+const greenButton = document.querySelector('.green');
+
+//will be set when button event listeners are clicked
+let color;
 
 
+
+button.addEventListener('click', setGrid);
+
+blueButton.addEventListener('click', () => color = 'blue');
+redButton.addEventListener('click', () => color = 'red');
+greenButton.addEventListener('click', () => color = 'green');
 
 function setGrid() {
     clear();
     let n = prompt("How many squares do you want per side?");
     createGrid(n);
+    //comment
     const gridItem = document.querySelectorAll('.grid-item');
     gridItem.forEach(item => item.addEventListener('mouseover', (e) => {
-        changeColor(e.target);
+        let first = firstRgbValue();
+        let second = secondRgbValue();
+        let third = thirdRgbValue();
+        changeColor(e.target, first, second, third);
     }
     ));
-    
 }
-
-button.addEventListener('click', setGrid);
 
 function createGrid(size) {
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -34,15 +47,40 @@ function createGrid(size) {
     }
 }
 
-function changeColor(element) {
-    element.classList.toggle('change-color');
+function changeColor(element, a, b, c) {
+    element.style.cssText = `background-color: rgb(${a}, ${b}, ${c});`;
 }
 
 
 function clear() {
     const div = document.querySelectorAll('.grid-item');
     div.forEach(i => gridContainer.removeChild(i));
-        
 }
 
+//random number generators to create a random color
+function firstRgbValue() {
+    return Math.floor(Math.random() * 256);
+}
 
+function secondRgbValue() {
+    return Math.floor(Math.random() * 256);
+}
+
+function thirdRgbValue() {
+    return Math.floor(Math.random() * 256);
+}
+//get random numbers from 0 to 255 and set that as color
+//have 3 functions that each return a number from 0 to 255
+//and set them equal to variables which will hold that value which we will use in rgb
+
+
+
+/*
+Create buttons which change the color of hover effect
+create global variable 'color' which gets set with a button event listener
+Then just pass that global variable 'color' into the changeColor function;
+
+
+
+
+*/
