3e837df70e5b3ffdb484a4a7b8d696730bba962f
Mikko <realmiks27@gmail.com>
2023-08-28 09:15:59 -0700

    Update grid functions to display the grid

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,36 +1,47 @@
 //try to add eslint and other things to format and make code look better and cleaner
 
-function setInitialGrid() {
+
+//maybe create a function that selects right container, creates grid container and then appends grid contaienr to right container
+//and then the fucntions will just add grid items to the grid container   
+
+export function setInitialGrid() {
+    createGridContainer();
     createGrid(16);
-    eventHandlers();
 }
 
-function setGridSize() {
+function createGridContainer() {
+    const gridContainer = document.createElement('div');
+    const rightContainer = document.querySelector('.right-container');
+    gridContainer.setAttribute('id', 'grid-container')
+
+    rightContainer.appendChild(gridContainer);    
+}
+
+export function setGridSize() {
     clear();
     let size = prompt("How many squares do you want per side?");
     createGrid(size);
 }
 
 function createGrid(size) {
-    //might have to create a grid container instead of selecting it
     const gridContainer = document.querySelector('#grid-container');
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    
     for (let i = 0; i < size ** 2; i++) { 
         const gridItem = document.createElement('div');
-        gridItem.classList.toggle('grid-item');
+        gridItem.classList.add('grid-item');
         gridContainer.appendChild(gridItem);
     }
-    displayGrid();
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;   
+    displayGrid(gridContainer);
 }
 
-function displayGrid() {
-    clear();
+function displayGrid(gridContainer) {
     const rightContainer = document.querySelector('.right-container');
-    const gridContainer = document.querySelector('#grid-container');
     rightContainer.appendChild(gridContainer);  
 }
 
 function clear() {
+    const gridContainer = document.querySelector('#grid-container');
     const gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach(gridItem => gridContainer.removeChild(gridItem));
 }
