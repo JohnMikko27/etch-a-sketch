277a9bc74b8405b57b58e17be4bbe81016580825
Mikko <realmiks27@gmail.com>
2023-08-28 07:46:21 -0700

    import functions into controller.js

---
+++ src/components/controller.js
@@ -0,0 +1,10 @@
+import { setInitialGrid, setGridSize } from "./grid.js";
+import { eventHandlers } from "./color.js";
+
+export function run() {
+    const changeGridButton = document.querySelector('#button');
+
+    setInitialGrid()
+    changeGridButton.addEventListener('click', setGridSize);
+    eventHandlers();
+}
