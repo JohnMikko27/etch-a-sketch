597ce45358bba775a4983c5b50400010cba4bda2
Mikko <realmiks27@gmail.com>
2023-08-30 13:32:22 -0700

    Merge branch 'main' of github.com:JohnMikko27/etch -a-sketch

--- src/components/controller.js
+++ src/components/controller.js
@@ -1,5 +1,5 @@
 import { setInitialGrid, setGridSize } from './grid';
-import { handlers } from './color';
+import handlers from './color';
 
 export default function run() {
   const changeGridButton = document.querySelector('#change-grid-button');
